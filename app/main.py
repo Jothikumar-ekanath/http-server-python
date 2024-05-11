53c75e235f4d5aafaf261d3e66700679087c6525
jothikumar <jothikumar.kuppaekanath@nike.com>
2024-05-10 17:57:54 -0700

    commit for stage 9

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ import argparse
 import os
 
 CRLF = "\r\n"
-args = None
-
+directory = "/files/"
 
 # TODO - refactor
 
@@ -12,7 +11,6 @@ async def produce_response(request: tuple) -> bytes:
     method, path, headers = request
     http_status = "200 OK"
     response_content = ""
-
     if method == "GET":
         if path == "/":
             pass
@@ -24,7 +22,6 @@ async def produce_response(request: tuple) -> bytes:
         elif "/files/" in path:
             pathArray = path.split("/")
             if len(pathArray) == 3:
-                directory = args.directory
                 fileName = pathArray[2]
                 path = directory + fileName
                 if os.path.isfile(path):
@@ -43,68 +40,82 @@ async def produce_response(request: tuple) -> bytes:
                 http_status = "404 Not Found"
         else:
             http_status = "404 Not Found"
-        headers = (
-            f"HTTP/1.1 {http_status}{CRLF}"
-            f"Content-Type: text/plain{CRLF}Content-Length: {len(response_content)}{CRLF}{CRLF}")
-        response_template = f"{headers}{response_content}"
-        return response_template.encode()  # "utf-8" default encoding
+        response_headers = prepare_response_headers(
+            http_status, len(response_content), headers)
+        response_template = f"{response_headers}{response_content}"
+        return response_template.encode()
 
 
-async def main():
-    server = await asyncio.start_server(connection_handler, "localhost", 4221)
-    print(f"Server running on {server.sockets[0].getsockname()}")
-    async with server:
-        await server.serve_forever()
+def prepare_response_headers(http_status, response_content_len, req_headers) -> str:
+    res_headers = f"HTTP/1.1 {http_status}{CRLF}Content-Type: text/plain{CRLF}Content-Length: {response_content_len}"
+    if 'Accept-Encoding' in req_headers:
+        encoding = req_headers['Accept-Encoding']
+        if encoding in ['gzip']:
+            res_headers += f"{CRLF}Content-Encoding: {encoding}"
+    res_headers += f"{CRLF}{CRLF}"
+    return res_headers
 
-
-async def connection_handler(
+async def connection_handler(   
         reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     try:
-        addr = writer.get_extra_info("peername")
+        addr = writer.get_extra_info('peername')
         while not reader.at_eof():
             payload = await reader.read(1024)
-            print(f"received payload: {payload}")
+            print(f'received payload: {payload}')
             if not payload:
                 break
-            header_lines, body = payload.decode().split("\r\n\r\n")
-            header_lines = header_lines.split("\r\n")
-            method, path, _ = header_lines[0].split(" ")
+            header_lines, body = payload.decode().split('\r\n\r\n')
+            header_lines = header_lines.split(CRLF)
+            method, path, _ = header_lines[0].split(' ')
             headers = {}
             for line in header_lines[1:]:
                 if line:
-                    key, value = line.split(": ")
+                    key, value = line.split(': ')
                     headers[key] = value
-            if method == "POST":
-                print(f"reading post {body}")
-                pathArray = path.split("/")
+            response = b''
+            if method == 'POST':
+                print(f'reading post {body}')
+                pathArray = path.split('/')
                 if len(pathArray) == 3:
-                    directory = args.directory
                     fileName = pathArray[2]
                     path = directory + fileName
-                    with open(path, "w") as file:
+                    with open(path, 'w') as file:
                         file.write(body)
-                    response = f"HTTP/1.1 201 OK{CRLF}Content-Type: text/plain{
-                        CRLF}Content-Length: 0{CRLF}{CRLF}"
-                    writer.write(response.encode())
-                    await writer.drain()
-            if method == "GET":
+                    response = f'HTTP/1.1 201 Created{CRLF}Content-Type: text/plain{
+                        CRLF}Content-Length: 0{CRLF}{CRLF}'
+                    response = response.encode()
+            if method == 'GET':
                 response = await produce_response((method, path, headers))
-                writer.write(response)
-                await writer.drain()
+            print(f'sending response: {response}')
+            writer.write(response)
+            await writer.drain()
+            
+            
+            
+            
     except asyncio.IncompleteReadError as e:
-        print(f"Connection with {addr} was closed")
+        print(f'Connection with {addr} was closed')
     except Exception as e:
-        print(f"An error occurred for {addr}: {e}")
+        print(f'An error occurred for {addr}: {e}')
     finally:
         writer.close()
+        await writer.wait_closed()
+
+async def main():
+    server = await asyncio.start_server(connection_handler, 'localhost', 4221)
+    print(f'Server running on {server.sockets[0].getsockname()}')
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
     parser = argparse.ArgumentParser()
-    parser.add_argument("--directory", help="serving files dir")
+    parser.add_argument('--directory',type=str)
     args = parser.parse_args()
+    if args.directory:
+        if not os.path.exists(args.directory):
+            os.makedirs(args.directory)
+        directory = args.directory
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        pass
+        print('Server stopped by ctrl-c')
